ddbb186a03458eb0fcf5dc218808703f678b5df4
HuiLi <1104349126@qq.com>
2020-05-30 12:08:14 +0800

    平级传值

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <FirstChild></FirstChild>
+    <SecondChild></SecondChild>
+  </div>
+</template>
+
+<script>
+import SecondChild from './views/SecondChild.vue'
+import FirstChild from './views/FirstChild.vue'
+
+export default {
+  name: 'App',
+  components: {
+   FirstChild,
+   SecondChild
+  }
+}
+</script>
+
+<style>
+@import "./assets/css/base.css";
+
+</style>
